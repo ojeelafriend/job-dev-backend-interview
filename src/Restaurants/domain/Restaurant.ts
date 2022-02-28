05da88b45e65e264391ce774254a42ac46c858b9
odev-badwin <ts8irolon@gmail.com>
2022-02-28 00:00:44 -0300

    Creo dos interfaces: Restaurant y RestaurantRepository
    Una estará ligada al sistema de persistencia y la otra de la forma que
    se crean los restaurantes, manejandose por ahora con primitivos.

---
+++ src/Restaurants/domain/Restaurant.ts
@@ -0,0 +1,6 @@
+export interface Restaurant {
+  urlImage: string;
+  restaurantName: string;
+  location: string;
+  openingTime: string;
+}
---
+++ src/Restaurants/domain/RestaurantRepository.ts
@@ -0,0 +1,6 @@
+import { Restaurant } from './Restaurant';
+
+export interface RestaurantRepository {
+  save(restaurant: Restaurant): Promise<any>;
+  checkRestaurant(restaurantName: string): Promise<boolean>;
+}
