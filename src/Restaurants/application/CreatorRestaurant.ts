8e574069c1b1831be0dbeab6b95e9d5af99d3df0
odev-badwin <ts8irolon@gmail.com>
2022-02-28 00:06:12 -0300

    En application, creo el caso de uso Creator y su validator

---
+++ src/Restaurants/application/CreatorRestaurant.ts
@@ -0,0 +1,27 @@
+import { Restaurant } from '../domain/Restaurant';
+import { RestaurantRepository } from '../domain/RestaurantRepository';
+import { Validator, ValidatorState } from './Validator';
+
+export class CreatorRestaurant extends Validator {
+  private repository: RestaurantRepository;
+
+  public constructor(repository: RestaurantRepository) {
+    super();
+    this.repository = repository;
+  }
+
+  public create(params: Restaurant): Promise<ValidatorState> {
+    let validatorState: ValidatorState;
+
+    return new Promise(async (resolve, reject) => {
+      validatorState = await this.validateParams(params);
+
+      if (!validatorState.state) reject(validatorState);
+
+      this.repository
+        .save(params)
+        .then(() => resolve({ message: 'Restaurant created', state: true }))
+        .catch((error) => reject({ message: error, state: false }));
+    });
+  }
+}
