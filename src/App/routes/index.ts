804018e5327f35a7378e5d08763b308545ee5da7
Alejo Solis <63030605+ojeelafriend@users.noreply.github.com>
2022-02-28 00:14:20 -0300

    Update index.ts

--- src/App/routes/index.ts
+++ src/App/routes/index.ts
@@ -2,8 +2,6 @@ import { Express } from 'express';
 
 import restaurant from './restaurant.routes';
 
-//localhost:3000/restaurant/register
-
 const routes = (server: Express) => {
   server.use('/restaurant', restaurant);
 };
